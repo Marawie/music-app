840442ff63db5d403bb4ecac4327fb80769fe04a
MarekMarczak <marawieeee@gmail.com>
2024-01-03 22:10:13 +0100

    Add new UserDto model

--- src/main/java/music/musicapp/dto/UserDto.java
+++ src/main/java/music/musicapp/dto/UserDto.java
@@ -13,21 +13,15 @@ import java.util.Set;
 @NoArgsConstructor
 public class UserDto {
 
-    @JsonProperty("id")
     private Long id;
 
-    @JsonProperty("username")
     private String username;
 
-    @JsonProperty("firstname")
     private String firstname;
 
-    @JsonProperty("lastname")
     private String lastname;
 
-    @JsonProperty("email")
     private String email;
 
-    @JsonProperty("friendships")
     private Set<User> userSet;
 }
