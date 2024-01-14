b50bf7923322fbffab915ff153a56df4ab41dbfa
MarekMarczak <marawieeee@gmail.com>
2024-01-14 18:50:19 +0100

    Add tests to PlaylistServiceImpl

--- src/main/java/music/musicapp/dto/PlaylistDto.java
+++ src/main/java/music/musicapp/dto/PlaylistDto.java
@@ -1,7 +1,9 @@
 package music.musicapp.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import music.musicapp.model.Music;
 import music.musicapp.model.user.User;
 
@@ -9,7 +11,10 @@ import java.util.Set;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlaylistDto {
+    private Long id;
     private boolean isPrivate;
     private String name;
     private User user;
---
+++ src/test/java/music/musicapp/service/PlaylistServiceImplTest.java
@@ -0,0 +1,67 @@
+package music.musicapp.service;
+
+import music.musicapp.dto.PlaylistDto;
+import music.musicapp.exception.RestException;
+import music.musicapp.model.Playlist;
+import music.musicapp.repository.PlaylistRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+
+class PlaylistServiceImplTest {
+
+    private PlaylistRepository playlistRepository;
+    private PlaylistServiceImpl playlistService;
+
+    @BeforeEach
+    void setUp() {
+        playlistRepository = mock(PlaylistRepository.class);
+        playlistService = new PlaylistServiceImpl(playlistRepository);
+    }
+    @Test
+    void changePrivacyStatus_ShouldUpdatePrivacyStatus() {
+
+        // Given
+        Long playlistId = 1L;
+        boolean isPrivate = true;
+
+        Playlist existingPlaylist = new Playlist();
+        existingPlaylist.setId(playlistId);
+        existingPlaylist.setPrivate(!isPrivate);
+
+        when(playlistRepository.findById(playlistId)).thenReturn(Optional.of(existingPlaylist));
+        when(playlistRepository.save(any(Playlist.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // When
+        PlaylistDto result = playlistService.changePrivacyStatus(playlistId, isPrivate);
+
+        // Then
+        verify(playlistRepository, times(1)).findById(playlistId);
+        verify(playlistRepository, times(1)).save(any(Playlist.class));
+        assertAll(() -> {
+            assert result != null;
+            assert result.getId().equals(playlistId);
+            assert result.isPrivate() == isPrivate;
+        });
+    }
+
+    @Test
+    void changePrivacyStatus_ShouldThrowExceptionIfPlaylistNotFound() {
+        // Given
+        Long playlistId = 1L;
+        boolean isPrivate = true;
+
+        when(playlistRepository.findById(playlistId)).thenReturn(Optional.empty());
+
+        //Then
+        assertThrows(RestException.class, () -> playlistService.changePrivacyStatus(playlistId, isPrivate));
+        verify(playlistRepository, times(1)).findById(playlistId);
+        verify(playlistRepository, never()).save(any(Playlist.class));
+    }
+}
