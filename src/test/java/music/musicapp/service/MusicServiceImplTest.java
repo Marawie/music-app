4ea27a6eeef0fa60e4a718d27410ede46d9aea4a
MarekMarczak <marawieeee@gmail.com>
2024-01-15 17:41:03 +0100

    add tests to MusicServiceImpl to verify restException

--- src/test/java/music/musicapp/service/MusicServiceImplTest.java
+++ src/test/java/music/musicapp/service/MusicServiceImplTest.java
@@ -1,6 +1,7 @@
 package music.musicapp.service;
 
 import music.musicapp.dto.PlaylistDto;
+import music.musicapp.exception.RestException;
 import music.musicapp.model.Music;
 import music.musicapp.model.Playlist;
 import music.musicapp.model.user.User;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import java.security.Principal;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class MusicServiceImplTest {
@@ -23,7 +25,7 @@ class MusicServiceImplTest {
     public void setUp() {
         userRepository = mock(UserRepository.class);
         playlistRepository = mock(PlaylistRepository.class);
-        playlistService = new MusicServiceImpl(userRepository,playlistRepository);
+        playlistService = new MusicServiceImpl(userRepository, playlistRepository);
     }
 
     @Test
@@ -48,5 +50,42 @@ class MusicServiceImplTest {
         verify(playlistRepository).findById(playlistId);
         verify(playlistRepository).save(playlist);
         assert result.isPrivate();
+    }
+
+    @Test
+    public void testAddMusicToPlaylistUserNotFound() {
+        // Given
+        Principal principal = mock(Principal.class);
+        Music music = new Music();
+        Long playlistId = 1L;
+
+        //When
+        when(userRepository.findByEmail(principal.getName())).thenReturn(Optional.empty());
+
+        //Then
+        assertThrows(RestException.class, () ->
+                playlistService.addMusicToPlaylist(principal, music, playlistId));
+
+        verifyNoInteractions(playlistRepository);
+        verify(playlistRepository, never()).save(any());
+    }
+
+    @Test
+    public void testAddMusicToPlaylistPlaylistNotFound() {
+        //Given
+        Music music = new Music();
+        Principal principal = mock(Principal.class);
+        Long playlistId = 1L;
+        User user = new User();
+
+        //When
+        when(userRepository.findByEmail(principal.getName())).thenReturn(Optional.of(user));
+        when(playlistRepository.findById(any())).thenReturn(Optional.empty());
+
+        //Then
+        assertThrows(RestException.class, () ->
+                playlistService.addMusicToPlaylist(principal, music, playlistId));
+
+        verify(playlistRepository, never()).save(any());
     }
 }
