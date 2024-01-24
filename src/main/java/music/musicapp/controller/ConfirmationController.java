ce6218d0c1a45eda85c733a398e0c6ed914f2fc2
MarekMarczak <marawieeee@gmail.com>
2024-01-24 21:18:07 +0100

    small changes in every method to make them more readable

--- src/main/java/music/musicapp/controller/ConfirmationController.java
+++ src/main/java/music/musicapp/controller/ConfirmationController.java
@@ -6,23 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 @RequiredArgsConstructor
 public class ConfirmationController {
     private final ConfirmationService confirmationService;
 
-
     @GetMapping("/confirm/{id}")
-    public String confirmRegistration(@PathVariable Long id, @RequestParam("token") String token, RedirectAttributes redirectAttributes) {
-        boolean confirmationResult = confirmationService.userEmailAccepted(id,token);
-
-        if (confirmationResult) {
-            return "redirect:/user/panel/";
-        } else {
-            redirectAttributes.addFlashAttribute("error", "Error during acceptance request.");
-            return "redirect:/exception/token-expired";
-        }
+    public void confirmRegistrationLink(@PathVariable Long id, @RequestParam("token") String token) {
+        confirmationService.userEmailAccepted(id, token);
     }
 }
